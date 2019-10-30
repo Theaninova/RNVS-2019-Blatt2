dc82d07e92338e69c3bb0c1b53c1d0a482e2106d
jean-p.walter <jean-pierre.walter@campus.tu-berlin.de>
2019-10-30 17:40:45 +0100

    client working

--- client.c
+++ client.c
@@ -1,7 +1,3 @@
-/*
- * A stream socket client demo
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -11,32 +7,29 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
-
 #include <arpa/inet.h>
 
-#define MAXDATASIZE 512 // max number of bytes we can get at once
+#define MAXDATASIZE 3 // max Data_length
 
-// get sockaddr, IPv4 or IPv6:
-void *get_in_addr(struct sockaddr *sa)
-{
+
+// get the IPv4 or IPv6 address
+void *get_in_addr(struct sockaddr *sa) {
     if (sa->sa_family == AF_INET) {
-        return &(((struct sockaddr_in*)sa)->sin_addr);
+        return &(((struct sockaddr_in *) sa)->sin_addr); //IPv4
     }
-
-    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+    return &(((struct sockaddr_in6 *) sa)->sin6_addr); //IPv6
 }
 
-int main(int argc, char *argv[])
-{
-    int sockfd, numbytes;
-    char buf[MAXDATASIZE];
-    struct addrinfo hints, *servinfo, *p;
+int main(int argc, char *argv[]) {
+    int socket_fd;
+    char buffer[MAXDATASIZE];
+    struct addrinfo hints, *server_info, *pointer;
     int rv;
     char s[INET6_ADDRSTRLEN];
 
     //error testing
     if (argc != 3) {
-        fprintf(stderr,"usage: client hostname, port\n");
+        perror("Error: please enter -> client hostname port\n");
         exit(1);
     }
 
@@ -45,49 +38,52 @@ int main(int argc, char *argv[])
     hints.ai_family = AF_UNSPEC; //IPv4 or IPv6
     hints.ai_socktype = SOCK_STREAM; //TCP Protocol
 
-    if ((rv = getaddrinfo(argv[1], argv[2], &hints, &servinfo)) != 0) {
+    if ((rv = getaddrinfo(argv[1], argv[2], &hints, &server_info)) != 0) {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
         return 1;
     }
-
-    // loop through all the results and connect to the first we can
-    for(p = servinfo; p != NULL; p = p->ai_next) {
-        if ((sockfd = socket(p->ai_family, p->ai_socktype,
-                             p->ai_protocol)) == -1) {
-            perror("client: socket");
+    for (pointer = server_info; pointer != NULL; pointer = pointer->ai_next) {
+        if ((socket_fd = socket(pointer->ai_family, pointer->ai_socktype,
+                                pointer->ai_protocol)) == -1) {
+            perror("Error: creating client socket");
             continue;
         }
-
-        if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-            perror("client: connect");
-            close(sockfd);
+        if (connect(socket_fd, pointer->ai_addr, pointer->ai_addrlen) == -1) {
+            perror("Error: no connection for client");
+            close(socket_fd);
             continue;
         }
-
         break;
     }
-
-    if (p == NULL) {
-        fprintf(stderr, "client: failed to connect\n");
+    if (pointer == NULL) {
+        fprintf(stderr, "Error: client failed to connect\n");
         return 2;
     }
 
-    inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),
+    inet_ntop(pointer->ai_family, get_in_addr((struct sockaddr *) pointer->ai_addr),
               s, sizeof s);
-    printf("client: connecting to %s\n", s);
+    printf("connecting to %s\n", s);
 
-    freeaddrinfo(servinfo); // all done with this structure
+    freeaddrinfo(server_info);
 
-    if ((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
-        perror("recv");
-        exit(1);
+    int total_number_of_bytes = 0;
+    int number_of_bytes;
+    char quote[1000];
+
+    while ((number_of_bytes = recv(socket_fd, buffer, MAXDATASIZE - 1, 0)) > 0) {
+        memcpy(quote + total_number_of_bytes, buffer, number_of_bytes);
+        total_number_of_bytes += number_of_bytes;
+        /*
+        else if ((number_of_bytes = recv(socket_fd, buffer, MAXDATASIZE - 1, 0)) == -1) {
+            perror("Error: fail to receive");
+            exit(1);
+        } */
     }
+    quote[total_number_of_bytes] = '\0';
 
-    buf[numbytes] = '\0';
+    printf("client: received '%s'\n", quote);
 
-    printf("client: received '%s'\n",buf);
-
-    close(sockfd);
+    close(socket_fd);
 
     return 0;
 }
