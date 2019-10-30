ff3a6e2c3cd2fd66f7b35068fe2bb0a2ccfa3f67
Jean-P.Walter <jean-pierre.walter@campus.tu-berlin.de>
2019-10-30 14:40:07 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Blatt2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "Blatt2.exe"
+  "Blatt2.exe.manifest"
+  "Blatt2.pdb"
+  "CMakeFiles/Blatt2.dir/client.c.obj"
+  "CMakeFiles/Blatt2.dir/server.c.obj"
+  "libBlatt2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Blatt2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
